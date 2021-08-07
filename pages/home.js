e50c0cd7c1a3e05cba1a0c42bd8e2643840714d4
Paul Chesa <paulchesa1@gmail.com>
2021-08-07 06:27:53 +0300

    Finished Sign Up form

--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styles from '../styles/Home.module.css'
 import Image from 'next/image'
+import Link from "next/link"
+
 
 function HomePage() {
     return (
@@ -19,22 +21,40 @@ function HomePage() {
                 <div className={styles.signUpForm}>
                     <form>
                         <div className="row">
+                        <h4>Sign Up</h4>
+                        </div>
+                        <div className={`${styles["inputRow"]} row`}>
                             <div className="col-6">
-                                <input type="text" placeholder="Enter First Name"/>
+                                <input className={styles.signUpFormInput} type="text" placeholder="Enter First Name" />
                             </div>
                             <div className="col-6">
-                                <input type="text" placeholder="Enter Second Name"/>
+                                <input className={styles.signUpFormInput} type="text" placeholder="Enter Second Name" />
                             </div>
                         </div>
-                        <div className="row">
+                        <div className={`${styles["inputRow"]} row`}>
                             <div className="col-12">
-                                <input type="email" placeholder="Enter Email"/>
+                                <input className={styles.signUpFormInput} type="email" placeholder="Enter Email" />
                             </div>
                         </div>
 
-                        <div className="row">
+                        <div className={`${styles["inputRow"]} row`}>
                             <div className="col-12">
-                                <input type="password" placeholder="Enter Password"/>
+                                <input className={styles.signUpFormInput} type="password" placeholder="Enter Password" />
+                            </div>
+                        </div>
+
+                        <div className={`${styles["inputRow"]} row`}>
+                            <div className="col-12">
+                                <button className={`${styles["signUpFormBtn"]} btn btn-success`} type="submit"> Sign Up </button>
+                            </div>
+                        </div>
+
+                        <div className={`${styles["inputRow"]} ${styles["bottomLinks"]} row`}>
+                            <div className="col-6">
+                                <Link href="/login"><a>Already have an account? Sign In Here</a></Link>
+                            </div>
+                            <div className="col-6">
+                                <Link href="/forgotPassword"><a>Forgot Password?</a></Link>
                             </div>
                         </div>
                     </form>
