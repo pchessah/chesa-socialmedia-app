805828dc7bb34aa60ffdb14c4f5f074ab1e2e9df
Paul Chesa <paulchesa1@gmail.com>
2021-08-08 06:58:08 +0300

    Sign Up,, log in, works

--- components/logOut.js
+++ components/logOut.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { userAuth } from '../pages/libs/context/userAuthContext';
+import { useRouter } from 'next/router';
 
 
 function LogOut() {
     const { signOut } = userAuth()
 
+    const router = useRouter()
+
     const logOut = () => {
         return signOut().then((result) => {
             window.alert("Signed Out")
+            router.push("/")
             
         }).catch((err) => {
             window.alert(err)
--- pages/login.js
+++ pages/login.js
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../styles/Home.module.css'
 import Link from "next/link"
 import { GoSignIn } from 'react-icons/go';
-import { useRouter} from "next/router"
+import { useRouter } from "next/router"
+import { userAuth } from "./libs/context/userAuthContext"
 
 function Login() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState(null)
+    const router = useRouter()
+    const {signInWithEmailAndPassword} = userAuth()
 
-    
+    const onLogIn = (event) => {
+        event.preventDefault()
+        setError(null)
+        signInWithEmailAndPassword(email, password).then((authUser) => {
+            router.push("/profile")
+        }).catch((error) => {
+            setError(error.message)
+        })
+    }
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value)
+    }
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value)
+    }
 
     return (
         <>
             <div className={styles.signUpForm}>
-                <form>
+                <form onSubmit={onLogIn}>
+                    {error && <Alert color="danger">{error}</Alert>}
                     <div className="row">
                         <div className="d-flex flex-column justify-content-center align-items-center col">
                             <GoSignIn />
@@ -21,13 +44,23 @@ function Login() {
 
                     <div className={`${styles["inputRow"]} row`}>
                         <div className="col-12">
-                            <input className={styles.signUpFormInput} type="email" placeholder="Enter Email" />
+                            <input
+                                className={styles.signUpFormInput}
+                                type="email"
+                                placeholder="Enter Email"
+                                value={email}
+                                onChange={handleEmailChange} />
                         </div>
                     </div>
 
                     <div className={`${styles["inputRow"]} row`}>
                         <div className="col-12">
-                            <input className={styles.signUpFormInput} type="password" placeholder="Enter Password" />
+                            <input
+                                className={styles.signUpFormInput}
+                                type="password"
+                                placeholder="Enter Password"
+                                value={password}
+                                onChange={handlePasswordChange} />
                         </div>
                     </div>
 
