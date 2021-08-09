36d8f6efac01d5e7b94799f9ea51cecd013239bc
Paul Chesa <paulchesa1@gmail.com>
2021-08-09 15:37:59 +0300

    Loader edited

--- components/loading.js
+++ components/loading.js
@@ -1,13 +1,21 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Backdrop from '@material-ui/core/Backdrop';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { makeStyles } from '@material-ui/core/styles';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 const useStyles = makeStyles((theme) => ({
     backdrop: {
         zIndex: theme.zIndex.drawer + 1,
         color: '#fff',
     },
+
+    root: {
+        width: '100%',
+        '& > * + *': {
+            marginTop: theme.spacing(5),
+        },
+    }
 }));
 
 function Loading() {
@@ -16,7 +24,10 @@ function Loading() {
     return (
         <>
             <Backdrop className={classes.backdrop} open={open}>
-                <CircularProgress color="inherit" />
+                <div className={classes.root}>
+                    <LinearProgress />
+                </div>
+
             </Backdrop>
         </>
 
